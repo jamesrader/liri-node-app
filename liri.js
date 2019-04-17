86287d2712c31b8326f52f8d123b906c25198e40
USCjames <james.rader@cox.net>
2019-04-17 12:52:56 -0700

    Concert-this complete. Added colors for terminal text.

--- liri.js
+++ liri.js
@@ -4,33 +4,29 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
+var colors = require("colors");
 var name = "";
 
 var command = process.argv[2];
 
 if (process.argv[3]){
     name = process.argv.slice(3).join(" ");
-    console.log(name);
 }
 
 switch (command){
     case "concert-this":
-    console.log("1");
     concertThis();
     break;
     
     case "spotify-this-song":
-    console.log("2");
     spotifyThis();
     break;
 
     case "movie-this":
-    console.log("3");
     movieThis();
     break;
 
     case "do-what-it-says":
-    console.log("4");
     doThis();
     break;
 
@@ -45,7 +41,28 @@ switch (command){
 }
 
 function concertThis(){
+    axios
+    .get("https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp")
+    .then(function(response){
 
+        if (response.data.length){
+        for (i=0; i<response.data.length; i++){
+            var item = response.data[i];
+        console.log("\nVenue: ".yellow + item.venue.name);
+        var location = "";
+        location += item.venue.city + ", ";
+        if (item.venue.region != ""){
+        location += item.venue.region + ", ";
+        }
+        location += item.venue.country;
+        console.log("Location: ".yellow + location);
+        console.log("Date: ".yellow + moment(item.datetime).format("MM/DD/YYYY"));
+        }
+    } else {
+        console.log (("\nNo upcoming performances found for " + name + ".").red);
+    }
+
+    })
 }
 
 function spotifyThis(){
